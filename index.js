89df4060b6f75267af45ec5759d7021dcf61d0a3
Zeref26 <killianpoulain26@gmail.com>
2019-04-04 21:57:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ bot.on('ready', () => {
     console.log('Bot is ready !');
 });
 
+bot.on('guildMemberAdd', member => {
+        //Captcha
+    let a = Math.floor(Math.random()*10)+1;
+});
+
+bot.on('message', message => {
+    if (message.content=='test') {
+        message.channel.send(Math.floor(Math.random()*10)+1);
+    }
+});
+
 bot.login(process.env.TOKEN);
